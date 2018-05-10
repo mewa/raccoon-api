541a232775581e5fb676f90f13b13854fb3266a2
mewa <marcin.k.chmiel@gmail.com>
2018-05-10 15:51:54 +0200

    Added reference to channel.k8s.io implementation

--- src/raccoon/ws/k8s/channel.clj
+++ src/raccoon/ws/k8s/channel.clj
@@ -1,9 +1,14 @@
+;; This file includes implementation of channel.k8s.io WebSocket subprotocol
+;; for mode details regarding the protocol see
+;; https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/apiserver/pkg/util/wsstream/conn.go#L34-L48
 (ns raccoon.ws.k8s.channel
   (:require [aleph.http :as http]
             [kubernetes.core :as core]
             [manifold.stream :as s]
             [clojure.core.async :refer [go <! >! <!! >!! chan thread close!]]))
 
+;; For respective channels refer to
+;; https://github.com/kubernetes/kubernetes/blob/5e4625cad72e5b9dde2f8a51eed06c59a6d70869/pkg/kubelet/server/remotecommand/websocket.go#L29-L53
 (def ^:const stream->channel
   {:stdin 0
    :stdout 1
