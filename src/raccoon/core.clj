3ff3c2ad4718c788d37bd2a4237406b4e6d5584b
mewa <marcin.k.chmiel@gmail.com>
2018-05-10 13:29:56 +0200

    Updated Kubernetes cluster config

--- src/raccoon/core.clj
+++ src/raccoon/core.clj
@@ -5,19 +5,23 @@
             [raccoon.api :as api]
             [ring.adapter.jetty :as jetty]))
 
-(defn incluster-config []
-  {:base-url "https://kubernetes.default.svc"
-   :debug true
-   :auths {"BearerToken" (str "Bearer " (slurp "/var/run/secrets/kubernetes.io/serviceaccount/token"))}})
-
-(defn test-config []
-  {:base-url "http://localhost:8001"})
+(defn load-config []
+  (merge {:base-url (System/getenv "KUBERNETES_URI")
+          :debug (System/getenv "RACCOON_DEBUG_K8S")}
+         (if-let [auth (System/getenv "RACCOON_AUTH")]
+           {:auths {"BearerToken" (str "Bearer " (slurp auth))}})))
 
 (defn run-k8s [f & args]
-  (core/with-api-context (test-config)
+  (core/with-api-context (load-config) (apply f args)))
+
+(defmacro in-k8s [body]
+  `(core/with-api-context (load-config) ~body))
+
+(defn try-k8s [f & args]
+  (in-k8s
     (try (apply f args)
          (catch Exception e (println {:error (or (ex-data e) e)})))))
 
 (defn -main
   [& args]
-  (jetty/run-jetty (partial run-k8s api/route-handler) {:port 4000}))
+  (jetty/run-jetty (partial try-k8s api/route-handler) {:port 4000}))
