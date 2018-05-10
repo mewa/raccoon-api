2ecbb182eba32ec0b6988176f5f702ad317728e0
mewa <marcin.k.chmiel@gmail.com>
2018-05-10 03:41:28 +0200

    Add stdin/out/err logic around api/attach calls

--- src/raccoon/api/attach.clj
+++ src/raccoon/api/attach.clj
@@ -1,16 +1,40 @@
 (ns raccoon.api.attach
   (:require [kubernetes.core :as core]
+            [kubernetes.api.core-v- :as k8scorev]
             [clojure.string :as s]
             [raccoon.ws.k8s.channel :as ws]
-            [clojure.core.async :refer [go <! >! <!! >!! chan thread]]))
+            [clojure.core.async :refer [go <! >! <!! >!! chan close!]]))
+
+(defn job-container-status [pod container]
+  (some #(if (= container (:name %)) (:state %))
+        (get-in (k8scorev/read-core-v1-namespaced-pod-status pod "default")
+                [:status :containerStatuses])))
 
 (defn attach
-  "Attaches to POD and returns channels for communication"
-  [namespace pod]
+  "Attaches to POD and returns [IN OUT] channels for communication"
+  [namespace pod container]
   (let [uri (str (s/replace-first (:base-url core/*api-context*) #"^http" "ws")
                  "/api/v1/namespaces/"
                  namespace
                  "/pods/"
                  pod
-                 "/attach?stdout=1&stderr=1&stdin=1")]
-    (ws/connect uri)))
+                 "/attach?stdout=1&stderr=1&stdin=1&container="
+                 container)]
+    (println "Waiting for container to become ready")
+    (loop [status (job-container-status pod container)]
+      (when (not (:running status))
+        (Thread/sleep 100)
+        (recur (job-container-status pod container))))
+    (println "Container is ready")
+    (println "Attaching to container")
+    (let [stdin (chan)
+          [in out] (ws/connect uri)]
+      ;; Initial empty message
+      (<!! out)
+      (go
+        (loop [msg (<! stdin)]
+          (when msg
+            (>! in {:type :stdin :msg (str msg "\n")})
+            (recur (<! stdin))))
+        (close! in))
+      [stdin out])))
