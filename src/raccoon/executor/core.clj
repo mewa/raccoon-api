c6e89e4bfff4b596c4261f2c84b0dc36fd0a20af
mewa <marcin.k.chmiel@gmail.com>
2018-05-11 03:02:36 +0200

    Add handling of exit codes to executor

--- src/raccoon/executor/core.clj
+++ src/raccoon/executor/core.clj
@@ -4,7 +4,15 @@
             [cheshire.core :refer :all]
             [raccoon.api.attach :as attach]
             [raccoon.core :as raccoon]
-            [clojure.core.async :refer [go timeout <! >! <!! >!! thread close! alts!!]]))
+            [clojure.core.async :refer [go timeout <! >! <!! >!! thread close! chan]]))
+
+(def eo-pipefail (str "set -o pipefail 2>/dev/null;" "set -e;"))
+
+(defn wrap-shell [cmd]
+  (str "sh -c '"
+       ;; Enable pipefail if underlying shell supports it
+       eo-pipefail
+       cmd "'"))
 
 (defn -main
   [& args]
@@ -14,11 +22,25 @@
      (let [steps (read-string (System/getenv "RACCOON_EXECUTOR_STEPS"))
            pod (System/getenv "RACCOON_POD_NAME")]
        (println "Performing steps:" steps)
-       (let [[in out] (attach/attach "default" pod "job")]
-         (doseq [step steps]
-           (println "Executing" step (>!! in step))
-           (println "Out" (<!! out)))
+       (let [[in out] (attach/attach "default" pod "job")
+             end (chan)]
+         (loop [step (first steps)
+                steps (rest steps)]
+           (when step
+             (let [step (wrap-shell step)]
+               (println "Executing" step (>!! in step))
+               (>!! in "LAST_CODE=$? && [ ! \"$LAST_CODE\" -eq 0 ] && echo Process exited with code $LAST_CODE && exit 0")
+               (go
+                 (loop [msg (<! out)]
+                   (when msg
+                     (if (= "Success" (msg "status"))
+                       (println "Success:" msg))
+                     (recur (<! out))))
+                 (close! end)))
+             (recur (first steps) (rest steps))))
+         (>!! in "exit 0")
          (close! in)
+         (<!! end)
          (close! out)))
      (catch Exception e (do
                           (println {:error (or (ex-data e) e)})
