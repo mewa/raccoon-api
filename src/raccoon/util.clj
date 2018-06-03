92125ae749833d65dd1674bbd9aa50c6c01077b0
mewa <marcin.k.chmiel@gmail.com>
2018-06-03 02:15:39 +0200

    Add blocking and non-blocking exhaust util functions

--- src/raccoon/util.clj
+++ src/raccoon/util.clj
@@ -1,9 +1,17 @@
 (ns raccoon.util
-  (:require [clojure.core.async :refer [<!!]]))
+  (:require [clojure.core.async :refer [<!! <!]]))
 
+(defn take-non-nil [seq]
+  (take-while #(not (nil? %)) seq))
 
-(defn exhaust
+(defn exhaust!!
   "Takes function FN which is applied to messages from channel CHAN until it's closed"
   [chan fn]
-  (doseq [msg (take-while #(not (nil? %)) (repeatedly #(<!! chan)))]
+  (doseq [msg (take-non-nil (repeatedly #(<!! chan)))]
+    (fn msg)))
+
+(defn exhaust!
+  "Takes function FN which is applied to messages from channel CHAN until it's closed"
+  [chan fn]
+  (doseq [msg (take-non-nil (repeatedly #(<! chan)))]
     (fn msg)))
